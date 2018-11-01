1d1126e65a177677ed4135037263522e856c9098
Ben Manu <bmanu10@gmail.com>
2018-11-02 06:45:31 +1300

    Axe module

--- themes/app/src/main.js
+++ themes/app/src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
+/**
+ * @see {@link https://github.com/vue-a11y/vue-axe}
+ */
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line
+  const VueAxe = require('vue-axe');
+  Vue.use(VueAxe, { config: {} });
+}
+
 Vue.config.productionTip = false;
 
 new Vue({
